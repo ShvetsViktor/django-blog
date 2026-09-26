8784af1caa0cb3a1cb34bbac2734af2241a7c54b
Viktor <shvetsviktor89@gmail.com>
2026-09-26 17:10:52 +0100

    create blog app

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-x6o^%gl3!sd@c5wq73fi9rpdfe$u03z_!!xjq2&suw%t@)tq_t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
