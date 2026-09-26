fee31dfdce9805b72150b2d80c4b77d00cd5377f
Viktor <shvetsviktor89@gmail.com>
2026-09-26 18:16:55 +0100

    change local and browser secret keys

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6o^%gl3!sd@c5wq73fi9rpdfe$u03z_!!xjq2&suw%t@)tq_t'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
